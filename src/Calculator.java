84be58b4675efc2ecd840f3d6d0feb96a59f6ef2
Karine Simonian <ks_05@mail.ru>
2022-09-20 21:15:48 +0500

    fix project structure

--- src/Calculator.java
+++ src/Calculator.java
@@ -8,7 +8,7 @@ public class Calculator {
     BinaryOperator<Integer> minus = (x, y) -> x - y;
     BinaryOperator<Integer> multiply = (x, y) -> x * y;
     BinaryOperator<Integer> devide = (x, y) -> y != 0 ? x / y : y / x;
-    //Выдает ошибку ArithmeticException в строке 8, т.е. при делении не учтено, что Y может быть 0
+    //Выдает ошибку ArithmeticException в строке 10, т.е. при делении не учтено, что Y может быть 0
 
     UnaryOperator<Integer> pow = x -> x * x;
     UnaryOperator<Integer> abs = x -> x > 0 ? x : x * -1;
